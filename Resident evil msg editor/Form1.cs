9b1874bb42b3ec7bcad601f51a49d84769ee5413
wolf3s <75867486+wolf3s@users.noreply.github.com>
2021-06-04 17:45:13 -0300

    Add dialog export

--- Resident evil msg editor/Form1.cs
+++ Resident evil msg editor/Form1.cs
@@ -30,16 +30,20 @@ namespace Resident_evil_msg_editor
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK);
             {
                 MessageBox.Show("msg reconhecido");
-                {
-                    MessageBox.Show("msg não reconhecido");
-                }
+                MessageBox.Show("msg não reconhecido");                
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Title = "Open Title";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save Title";
+            sfd.Filter = "msg file|*.msg *.MSG .*MSG2|MSG|*.MSG|MSG2|* .MSG2";
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK);
+            {
+                MessageBox.Show("msg salvo com sucesso");
+                MessageBox.Show("msg foi salvo sem sucesso");
+            }
         }
     }
 }
