48b34d76d8cd12863c847627922d252b4fa34d15
Wolfywolfy <75867486+wolfysdl@users.noreply.github.com>
2021-06-03 22:57:20 -0300

    Add files via upload

---
+++ Resident evil msg editor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Resident_evil_msg_editor
+{
+    partial class Tela1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Importar arquivo msg ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sigmar One", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 53);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resident evil msg v0.1 ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(558, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Exportar/Compilar msg";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Tela1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Tela1";
+            this.Text = "RESIDENT EVIL MSG EDITOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Resident evil msg editor/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resident_evil_msg_editor
+{
+    public partial class Tela1 : Form
+    {
+        public Tela1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Title";
+            ofd.Filter = "msg file|*.msg *.MSG .*MSG2|MSG|*.MSG|MSG2|* .MSG2";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK);
+            {
+                MessageBox.Show("msg reconhecido");
+                MessageBox.Show("msg reconhecido");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Title";
+        }
+    }
+}
